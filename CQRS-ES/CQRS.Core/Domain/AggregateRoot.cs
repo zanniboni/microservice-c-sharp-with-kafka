d261bbcca0019bbc909785e191923aa9492c134f
Vinicius Zaniboni Chagas <Vinicius.ZaniboniChagas-ext@ab-Inbev.com>
2022-11-03 11:17:46 -0300

    Added aggregate root

---
+++ CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,47 @@
+using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain
+{
+    public abstract class AggregateRoot
+    {
+        protected Guid _id; 
+        private readonly List<BaseEvent> _changes = new();
+        public Guid Id{
+            get { return _id; }
+        }
+        
+        public int Version { get; set; } = -1;
+
+        public IEnumerable<BaseEvent> GetUncommittedChanges() {
+            return _changes;
+        }
+
+        public void MarkChangesAsCommited() {
+            _changes.Clear();
+        }
+
+        private void ApplyChanges(BaseEvent @event, bool isNew) {
+            var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+
+            if(method == null) {
+                throw new ArgumentException(nameof(method), $"The Apply method was not found in the aggregate root for {@event.GetType().Name}!");
+            }
+            method.Invoke(this, new object[] { @event });
+
+            if (!isNew) {
+                _changes.Add(@event);
+            }
+        }
+
+        protected void RaiseEvent(BaseEvent @event) {
+            ApplyChanges(@event, true);
+        }
+
+        public void ReplayEvents(IEnumerable<BaseEvent> events) {
+            foreach(var @event in events) {
+                ApplyChanges(@event, false);
+            }
+        }
+
+    }
+}
