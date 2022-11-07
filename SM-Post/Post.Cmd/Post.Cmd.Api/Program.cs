77fc22ca9642a21cf569b4a5417f40e2a0fb8c4e
Vinicius Zaniboni <vzaniboni@ciandt.com>
2022-11-07 10:42:36 -0300

    Added CommandHandling

--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,7 +1,14 @@
 
+using System.Windows.Input;
 using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
 using CQRS.Core.Infraestructure;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
+using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
 
@@ -11,6 +18,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection(nameof(MongoDBConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventsStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// Register Command Handler Methods
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Handlers/EventSourcingHandler.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Handlers/EventSourcingHandler.cs
@@ -9,16 +9,17 @@ namespace Post.Cmd.Infrastructure.Handlers
     {
         private readonly IEventStore _eventStore;
 
-        public EventSourcingHandler(IEventStore eventStore) {
+        public EventSourcingHandler(IEventStore eventStore)
+        {
             _eventStore = eventStore;
         }
-        
+
         public async Task<PostAggregate> GetByIdAsync(Guid aggregateId)
         {
             var aggregate = new PostAggregate();
             var events = await _eventStore.GetEventAsync(aggregateId);
 
-            if(events == null && !events.Any()) return aggregate;
+            if (events == null && !events.Any()) return aggregate;
 
             aggregate.ReplayEvents(events);
             var latestVersion = events.Select(x => x.Version).Max();
@@ -29,7 +30,7 @@ namespace Post.Cmd.Infrastructure.Handlers
         public async Task SaveAsync(AggregateRoot aggregate)
         {
             await _eventStore.SaveEventAsync(aggregate.Id, aggregate.GetUncommittedChanges(), aggregate.Version);
-            aggregate.MarkChangesAsCommited();            
+            aggregate.MarkChangesAsCommited();
         }
     }
 }
